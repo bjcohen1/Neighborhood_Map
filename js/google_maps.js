669437c6a36b2dad4ecc9990cf9fa04395e80def
Barry <bjcohen3@gmail.com>
2017-06-20 16:13:58 -0400

    Added foursquare info to populate google maps infowindows

--- js/google_maps.js
+++ js/google_maps.js
@@ -4,31 +4,32 @@ var mapLocations = [
     {
      name:'Louisville Slugger Bat Factory',
      LatLng: {lat: 38.2570969, lng: -85.7449177},
-     description:'Test1',
-     locationID: '4b4261d2f964a520c3d225e3'
+     //description:'Sunday: + Monday:',
+     venueID: '4b4261d2f964a520c3d225e3'
     },
     {
      name:'Churchill Downs Racetrack',
      LatLng: {lat: 38.2029725, lng: -85.772227},
-     description:'Test2',
-     locationID: '4b0933a7f964a520b31423e3'
+     //description:'Test2',
+     venueID: '4b0933a7f964a520b31423e3'
     },
-    {name: 'Muhammad Ali Center',
+    {
+     name: 'Muhammad Ali Center',
      LatLng: {lat: 38.2582229, lng: -85.7620477},
-     description: 'Test3',
-     locationID: '4b3134f0f964a5209b0225e3'
+     //description: 'Test3',
+     venueID: '4b3134f0f964a5209b0225e3'
     },
     {
-      name:'Louisville International Airport',
-      LatLng: {lat: 38.175662, lng: -85.7391118},
-      description: 'Test4',
-      locationID: '4b0363e8f964a5201f4f22e3'
+     name:'Louisville International Airport',
+     LatLng: {lat: 38.175662, lng: -85.7391118},
+     //description: 'Test4',
+     venueID: '4b0363e8f964a5201f4f22e3'
     },
     {
-      name:'Belle of Louisville',
-      LatLng: {lat: 38.259186, lng: -85.7577707},
-      description: 'Test5',
-      locationID: '4ba7d023f964a5208fb539e3'
+     name:'Belle of Louisville',
+     LatLng: {lat: 38.259186, lng: -85.7577707},
+     //description: 'Test5',
+     venueID: '4ba7d023f964a5208fb539e3'
     }
     ];
 
@@ -41,14 +42,18 @@ var mapLocations = [
 
         //use marker to show the listing
 
-        var infowindow = new google.maps.InfoWindow();
-        var i, marker;
+        var i, marker, position, title, fourSquareUrl, infowindow;
 
-        var markerSet = [];
+        var clientID = "PJAU3L5FYIPZLSZ0O0IMNLPOMYCDFW3I5JPUKNUDI1TI3BIZ";
+        var clientSecret = "D45X0FQUAR2ZXCOW2CSF1KZEZAZLW0NGOWLYTBQWBIFYQ4KX";
+        var v = "20170619";
+        var m = "foursquare";
+
+        infowindow = new google.maps.InfoWindow();
 
         for (i = 0; i < mapLocations.length; i++) {
-          var position = new google.maps.LatLng(mapLocations[i].LatLng);
-          var title = mapLocations[i].name;
+          position = new google.maps.LatLng(mapLocations[i].LatLng);
+          title = mapLocations[i].name;
 
           marker = new google.maps.Marker({
             position : position,
@@ -57,8 +62,11 @@ var mapLocations = [
             animation: google.maps.Animation.DROP
           });
 
-          markerSet.push(marker);
-          mapLocations[i].marker = markerSet[i];
+          mapLocations[i].marker = marker;
+
+          fourSquareUrl = "https://api.foursquare.com/v2/venues/" + mapLocations[i].venueID + "?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=" + v + "&m=" + m;
+
+          mapLocations[i].fourSquareUrl = fourSquareUrl;
 
         // Add a closure on the event listener to assign unique values to
         // each of the markers instead of the value of the last one falling through
@@ -68,6 +76,8 @@ var mapLocations = [
           google.maps.event.addListener(infowindow, 'closeclick', showMarkers)
         };
 
+        console.log(mapLocations[1].fourSquareUrl);
+
         function clearMarkers() {
           for (var i = 0; i < mapLocations.length; i++) {
                 mapLocations[i].marker.setVisible(false);
@@ -76,15 +86,23 @@ var mapLocations = [
 
         function clickMarker(marker, i) {
           return function () {
-              clearMarkers();
-              mapLocations[i].marker.setVisible(true);
-              infowindow.setContent(mapLocations[i].description);
-              infowindow.open(map, marker);
-              // this line will change color and style of marker when clicked, need to add function to
-              // keep track of when a specific icon is clicked or not clicked
-              marker.setAnimation(google.maps.Animation.BOUNCE);
-            }
-          }(marker, i)
+            clearMarkers();
+            infowindow.setContent(null);
+            mapLocations[i].marker.setVisible(true);
+
+            $.ajax({
+              //url: "https://api.foursquare.com/v2/venues/4b4261d2f964a520c3d225e3?client_id=PJAU3L5FYIPZLSZ0O0IMNLPOMYCDFW3I5JPUKNUDI1TI3BIZ&client_secret=D45X0FQUAR2ZXCOW2CSF1KZEZAZLW0NGOWLYTBQWBIFYQ4KX&v=20170101&m=foursquare",
+              url: mapLocations[i].fourSquareUrl,
+              success: function(data) {
+                console.log(data.response);
+              infowindow.setContent(data.response.venue.name);
+              }
+            });
+
+            infowindow.open(map, marker);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+        }(marker, i)
 
         function showMarkers() {
           for (var i = 0; i < mapLocations.length; i++) {
@@ -93,16 +111,5 @@ var mapLocations = [
           }
         }
 
-        function loadFourSquare() {
-          var clientID = "PJAU3L5FYIPZLSZ0O0IMD45X0FQUAR2ZXCOW2CSF1KZEZAZLW0NGOWLYTBQWBIFYQ4KXNLPOMYCDFW3I5JPUKNUDI1TI3BIZ";
-          var clientSecret = "D45X0FQUAR2ZXCOW2CSF1KZEZAZLW0NGOWLYTBQWBIFYQ4KX";
-          var v = "20170619";
-          var m = "foursquare";
-
-          for (i = 0; i < mapLocations.length; i++) {
-            var fourSquareUrl = "https://api.foursquare.com/v2/venues/" +
-            mapLocations[i].venueID + "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=" + v;
-        }
-        }
         ko.applyBindings(new ViewModel());
     }
