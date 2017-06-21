6ca3caedd2ca6d05d4ccaa5332fcc45d5719a24d
Barry <bjcohen3@gmail.com>
2017-06-21 15:07:43 -0400

    Fixed Google Maps Error Handling

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
-
-/*var Location = function (data) {
-    this.name = ko.observable(data.name);
-    this.marker = data.marker;
-}*/
-
 var ViewModel = function() {
     var self = this;
 
+    //save all of the locations as an observable array to allow for filtering
     this.locations = ko.observableArray(mapLocations);
 
+    //opens an infowindow if a location name is clicked
+    //as if the marker for the location was clicked
     this.clickedOn = function (location) {
         google.maps.event.trigger(location.marker, 'click');
     }
 
+    //implement the filter functionality
+    //use an internal knockout function, arrayFilter, to check
+    //every element in the locations array against the search term
+    //input by the user in the filter bar
     this.filter = ko.observable("");
 
     this.filteredItems = ko.computed(function() {
         var filter = self.filter().toLowerCase();
+         //if there are no results to display in the filter observable, show all
+            //the location names and all of the markers
         if (!filter) {
             for (i = 0; i < this.locations().length; i++) {
                 this.locations()[i].marker.setVisible(true);
             }
             return self.locations();
+        //if the user input matches the name of one or more locations,
+        //only display those location names and markers on the map
         } else {
            return ko.utils.arrayFilter(self.locations(), function(item) {
                 var results = item.name.toLowerCase().indexOf(filter) > -1;
@@ -35,4 +40,3 @@ var ViewModel = function() {
         }
     }, self);
 }
-
--- js/google_maps.js
+++ js/google_maps.js
@@ -4,45 +4,45 @@ var mapLocations = [
     {
      name:'Louisville Slugger Bat Factory',
      LatLng: {lat: 38.2570969, lng: -85.7449177},
-     //description:'Sunday: + Monday:',
      venueID: '4b4261d2f964a520c3d225e3'
     },
     {
      name:'Churchill Downs Racetrack',
      LatLng: {lat: 38.2029725, lng: -85.772227},
-     //description:'Test2',
      venueID: '4b0933a7f964a520b31423e3'
     },
     {
      name: 'Muhammad Ali Center',
      LatLng: {lat: 38.2582229, lng: -85.7620477},
-     //description: 'Test3',
      venueID: '4b3134f0f964a5209b0225e3'
     },
     {
      name:'Louisville International Airport',
      LatLng: {lat: 38.175662, lng: -85.7391118},
-     //description: 'Test4',
      venueID: '4b0363e8f964a5201f4f22e3'
     },
     {
      name:'Belle of Louisville',
      LatLng: {lat: 38.259186, lng: -85.7577707},
-     //description: 'Test5',
      venueID: '4ba7d023f964a5208fb539e3'
     }
     ];
+      function mapFail() {
+        window.alert("Google Maps Has Failed to Load.  Maybe Go Watch YouTube or SomeThing")
+      }
 
       function initMap() {
-        // Constructor creates a new map - where in html to create the map and only center and zoom are required.
+        // Constructor creates a new map - at the html element with id = map and centered at given lat and lng.
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 38.2570969, lng: -85.7449177},
           zoom: 12
         });
 
+        //Draw the button to open the filter functionality on smaller screens
         var toggle = document.getElementById('open-btn');
         map.controls[google.maps.ControlPosition.LEFT_TOP].push(toggle);
 
+        //Initialize the variables for creating markers as well as using the FourSquare API
         var i, marker, position, title, fourSquareUrl, infowindow;
 
         var clientID = "PJAU3L5FYIPZLSZ0O0IMNLPOMYCDFW3I5JPUKNUDI1TI3BIZ";
@@ -52,6 +52,7 @@ var mapLocations = [
 
         infowindow = new google.maps.InfoWindow();
 
+        //Create a marker for each location above and save the marker as a attribute of its place
         for (i = 0; i < mapLocations.length; i++) {
           position = new google.maps.LatLng(mapLocations[i].LatLng);
           title = mapLocations[i].name;
@@ -65,6 +66,7 @@ var mapLocations = [
 
           mapLocations[i].marker = marker;
 
+          //Save the FourSquare API requset for each location as an attribute of the location
           fourSquareUrl = "https://api.foursquare.com/v2/venues/" + mapLocations[i].venueID + "?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=" + v + "&m=" + m;
 
           mapLocations[i].fourSquareUrl = fourSquareUrl;
@@ -73,22 +75,24 @@ var mapLocations = [
         // each of the markers instead of the value of the last one falling through
         // Thanks to stack exchange for some help with the details!
         // https://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-multiple-marker-example
+        //Add listeners for when the markers are clicked and when infowindows are closed
           google.maps.event.addListener(marker, 'click', clickMarker(marker, i))
           google.maps.event.addListener(infowindow, 'closeclick', showMarkers)
         };
 
-        console.log(mapLocations[1].fourSquareUrl);
-
+        //Resize and recenter the map whenever it is resized
         google.maps.event.addDomListener(window, 'resize', function() {
           map.setCenter(map.center);
         });
 
+        //Function to clear all of the markers off the map during filtering/selection markers
         function clearMarkers() {
           for (var i = 0; i < mapLocations.length; i++) {
                 mapLocations[i].marker.setVisible(false);
               }
         }
 
+        //Function to set infowindow content from the FourSquare API and open infowindows when clicked
         function clickMarker(marker, i) {
           return function () {
             clearMarkers();
@@ -121,6 +125,7 @@ var mapLocations = [
           }
         }(marker, i)
 
+        //Displays all of the markers for the registered locations
         function showMarkers() {
           for (var i = 0; i < mapLocations.length; i++) {
             mapLocations[i].marker.setAnimation(null);
@@ -128,6 +133,7 @@ var mapLocations = [
           }
         }
 
-        //window.alert("Failed to load map. We'll do our best to get it up and running for you!")
         ko.applyBindings(new ViewModel());
     }
+
+    //Prompt the user in case the map fails to load
